b47e7833bd8eb5183ae330a65347ad9fc5e5345d
priyanka1315096 <priyankachamp1996@gmail.com>
2016-08-20 10:10:18 +0530

    Add files via upload

---
+++ poower.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class power {
+	public static void main(String[] args) {
+	 int i;
+	 Scanner in=new Scanner(System.in);
+	 System.out.print("Enter the number:");
+	  i=in.nextInt();
+	  System.out.println("The power of the number is "+i*i);
+	}
+
+}
