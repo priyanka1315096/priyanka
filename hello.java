e76d9b40831b00c1e18de47900efd9751876999c
priyanka1315096 <priyankachamp1996@gmail.com>
2016-08-20 09:55:16 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class helloo {
+	public static void main(String[] args) {
+	  int i,n;
+	  Scanner in=new Scanner(System.in);
+	  System.out.print("Enter the number:");
+	  n=in.nextInt();
+	  for(i=0;i<n;i++)
+         System.out.println("hello");
+	}
+
+}
